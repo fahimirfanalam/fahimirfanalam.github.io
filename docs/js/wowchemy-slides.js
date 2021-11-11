8808f2969ebea209f1ffaa8501e07805614dea14
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2021-11-11 17:26:23 +0600

    deployed

--- docs/js/wowchemy-slides.js
+++ docs/js/wowchemy-slides.js
@@ -1,6 +1,6 @@
 (() => {
   // ns-params:@params
-  var slides = {highlight_style: "dracula", theme: "black"};
+  var slides = { highlight_style: "dracula", theme: "black" };
 
   // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20210413192202-ab8d28988e0e/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
